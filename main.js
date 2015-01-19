fe9bc7e768395fe0bd7c998873da932561929a97
Nick Fredmangit config --global ntfredman@gmail.comgit config --global user.name Nick <ntfredman@gmail.com>
2015-01-19 12:29:46 -0700

    Solutions to all loops added

---
+++ main.js
@@ -0,0 +1,29 @@
+for(var i=0; i<=500; i+=100) {
+  console.log(i);
+}
+
+for(var i=1; i<=64; i=i*2) {
+  console.log(i);
+}
+
+for(var i=3; i<=11; i++) {
+  console.log(Math.floor(i/3));
+}
+
+for(var i=0; i<=10; i+=2) {
+  console.log(i);
+}
+
+for(var i=3; i<=15; i+=3) {
+  console.log(i);
+}
+
+for(var i=9; i>=0; i--) {
+  console.log(i);
+}
+
+for(var i=0; i<=2; i++) {
+  for(var x=0; x<=3; x++) {
+    console.log(x);
+  }
+}
